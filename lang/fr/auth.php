bb7f6f6283760225a73dd81794f68759e50600ab
Léo LCL. CLERC <lcl@mtb.aigle>
2026-02-06 16:02:21 +0100

    SECURISATION DE L’AUTHENTIFICATION

---
+++ lang/fr/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'failed' => 'Identifiant ou mot de passe incorrect.',
+    'password' => 'Identifiant ou mot de passe incorrect.',
+    'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez réessayer dans :minutes minute(s).',
+
+];
